8d36859183b9190ce3215de2a7fd30f17d4561c8
Mode8 <gateguy33@gmail.com>
2024-02-22 21:59:28 -0500

    Adjusted includes

--- SuDokuLVS2019/src/include_graphics.h
+++ SuDokuLVS2019/src/include_graphics.h
@@ -10,16 +10,44 @@
 #include "graphics_include/grid_mini_bottom_right.h"
 #include "graphics_include/grid_mini_top_left.h"
 #include "graphics_include/grid_mini_top_right.h"
-#include "graphics_include/logo_1080.h"
-#include "graphics_include/logo_1440.h"
-#include "graphics_include/logo_2160.h"
-#include "graphics_include/logo_240.h"
-#include "graphics_include/logo_272.h"
-#include "graphics_include/logo_480.h"
-#include "graphics_include/logo_544.h"
-#include "graphics_include/logo_720.h"
 #include "graphics_include/menu_cursor.h"
 #include "graphics_include/sidebar_small.h"
 #include "graphics_include/tile.h"
+#include "graphics_include/logo_240.h"
+#include "graphics_include/logo_272.h"
+
+#if defined(PSP)
+#define logo_480_png NULL
+#define logo_480_png_len 0
+#define logo_544_png NULL
+#define logo_544_png_len 0
+#else
+#include "graphics_include/logo_480.h"
+#include "graphics_include/logo_544.h"
+#endif
+
+#if defined(PSP) || defined(VITA)
+#define logo_720_png NULL
+#define logo_720_png_len 0
+#else
+#include "graphics_include/logo_720.h"
+#endif
+
+#if defined(PSP) || defined(VITA) || defined(WII_U)
+#define logo_1080_png NULL
+#define logo_1080_png_len 0
+#else
+#include "graphics_include/logo_1080.h"
+#endif
+
+#if defined(PSP) || defined(VITA) || defined(WII_U) || defined(SWITCH)
+#define logo_1440_png NULL
+#define logo_1440_png_len 0
+#define logo_2160_png NULL
+#define logo_2160_png_len 0
+#else
+#include "graphics_include/logo_1440.h"
+#include "graphics_include/logo_2160.h"
+#endif
 
 #endif
